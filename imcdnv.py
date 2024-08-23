06947995abf023cfc994f20863bc0d564ff931f2
Gustavo Nicoletti <gus.nicoletti@gmail.com>
2024-08-23 18:34:20 -0300

    n salvei sa misera

---
+++ imcdnv.py
@@ -0,0 +1,15 @@
+peso = float("Seu peso: ")
+altura = float("Sua altura: ")
+
+imc = peso / altura
+
+if imc <=18.4:
+    print("abaixo do peso")
+elif imc <= 25:
+    print("peso normal")
+elif imc <= 30:
+    print("acima do peso")
+elif imc <= 35:
+    print("obesidade")
+elif imc >35:
+    print("obesisade acima do segundo grau")
